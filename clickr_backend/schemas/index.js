c45a0cb0c078525938602426f0739e1810445a77
Iviwe Davids <iviwedavids@gmail.com>
2023-05-06 00:26:34 +0200

    second commit

---
+++ clickr_backend/schemas/index.js
@@ -0,0 +1,7 @@
+export const schemaTypes = [comment, user, pin, postedBy, save]
+
+import comment from './comment'
+import user from './user'
+import pin from './pin'
+import postedBy from './postedBy'
+import save from './save'
